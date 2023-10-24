7e6571004db3ef4fc00d0063394e3149a5ef9dea
stanleyneko <ebisumio@gmail.com>
2023-10-24 22:02:25 +0100

    published order changed

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request,'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
